a44235b07cef3851a55c960dac0598d676302091
John <johndarkwah@live.it>
2024-08-21 08:08:49 +0100

    rules

---
+++ KataTest_Domain/domain/Rule.cs
@@ -0,0 +1,17 @@
+﻿namespace KataTest_Domain.domain
+{
+    public class Rule
+    {
+        public int UnitPrice { get; }
+        public int? SpecialQuantity { get; }
+        public int? SpecialPrice { get; }
+
+        public Rule(int unitPrice, int? specialQuantity = null, int? specialPrice = null)
+        {
+            UnitPrice = unitPrice;
+            SpecialQuantity = specialQuantity;
+            SpecialPrice = specialPrice;
+        }
+    }
+}
+}
---
+++ KataTest_Domain/repos/Checkout.cs
@@ -0,0 +1,56 @@
+﻿using KataTest_Domain.domain;
+using KataTest_Domain.interfaces;
+
+namespace KataTest_Domain.repos
+{
+    public class Checkout : ICheckout
+    {
+        private readonly Dictionary<string, Rule> rules;
+        private readonly Dictionary<string, int> _items;
+
+        public Checkout(Dictionary<string, Rule> pricingRules)
+        {
+            rules = pricingRules;
+            _items = new Dictionary<string, int>();
+        }
+
+        public void Scan(string item)
+        {
+            if (_items.ContainsKey(item))
+            {
+                _items[item]++;
+            }
+            else
+            {
+                _items[item] = 1;
+            }
+        }
+
+        public int GetTotalPrice()
+        {
+            int totalPrice = 0;
+
+            foreach (var item in _items)
+            {
+                var sku = item.Key;
+                var quantity = item.Value;
+                var rule = rules[sku];
+
+                if (rule.SpecialQuantity.HasValue && rule.SpecialPrice.HasValue)
+                {
+                    int specialBundles = quantity / rule.SpecialQuantity.Value;
+                    int remainingItems = quantity % rule.SpecialQuantity.Value;
+
+                    totalPrice += specialBundles * rule.SpecialPrice.Value;
+                    totalPrice += remainingItems * rule.UnitPrice;
+                }
+                else
+                {
+                    totalPrice += quantity * rule.UnitPrice;
+                }
+            }
+
+            return totalPrice;
+        }
+    }
+}
