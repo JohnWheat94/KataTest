c2beebaa84496f6d2c3b3d928d6fdd5e448d8e33
John <johndarkwah@live.it>
2024-08-21 08:20:59 +0100

    pricing test

---
+++ KataTest_Test/Test.cs
@@ -0,0 +1,48 @@
+using KataTest_Domain.domain;
+using KataTest_Domain.interfaces;
+using KataTest_Domain.repos;
+
+namespace KataTest_Test
+{
+    public class Test
+    {
+        [Fact]
+        public void Test_SingleItem_Price()
+        {
+            var pricingRules = new Dictionary<string, Rule>
+            {
+                { "A", new Rule(50, 3, 130) },
+                { "B", new Rule(30, 2, 45) },
+                { "C", new Rule(20) },
+                { "D", new Rule(15) }
+            };
+
+            ICheckout checkout = new Checkout(pricingRules);
+            checkout.Scan("A");
+            Assert.Equal(50, checkout.GetTotalPrice());
+        }
+
+        [Fact]
+        public void Test_MultipleItems_WithSpecialPrice()
+        {
+            var pricingRules = new Dictionary<string, Rule>
+        {
+            { "A", new Rule(50, 3, 130) },
+            { "B", new Rule(30, 2, 45) },
+            { "C", new Rule(20) },
+            { "D", new Rule(15) }
+        };
+
+            ICheckout checkout = new Checkout(pricingRules);
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A"); // Special price should apply: 3 for 130
+            checkout.Scan("B");
+            checkout.Scan("B"); // Special price should apply: 2 for 45
+            checkout.Scan("C");
+
+            Assert.Equal(195, checkout.GetTotalPrice()); // 130 (A) + 45 (B) + 20 (C)
+        }
+
+    }
+}
