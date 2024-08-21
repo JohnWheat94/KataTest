74034e0f0d519f111b6d1c43b55931ea3ba625b6
John <johndarkwah@live.it>
2024-08-21 08:22:26 +0100

    test mixed items

--- KataTest_Test/Test.cs
+++ KataTest_Test/Test.cs
@@ -26,12 +26,12 @@ namespace KataTest_Test
         public void Test_MultipleItems_WithSpecialPrice()
         {
             var pricingRules = new Dictionary<string, Rule>
-        {
-            { "A", new Rule(50, 3, 130) },
-            { "B", new Rule(30, 2, 45) },
-            { "C", new Rule(20) },
-            { "D", new Rule(15) }
-        };
+            {
+                { "A", new Rule(50, 3, 130) },
+                { "B", new Rule(30, 2, 45) },
+                { "C", new Rule(20) },
+                { "D", new Rule(15) }
+            };
 
             ICheckout checkout = new Checkout(pricingRules);
             checkout.Scan("A");
@@ -44,5 +44,25 @@ namespace KataTest_Test
             Assert.Equal(195, checkout.GetTotalPrice()); // 130 (A) + 45 (B) + 20 (C)
         }
 
+
+        [Fact]
+        public void Test_MixedItems()
+        {
+            var pricingRules = new Dictionary<string, Rule>
+            {
+                { "A", new Rule(50, 3, 130) },
+                { "B", new Rule(30, 2, 45) },
+                { "C", new Rule(20) },
+                { "D", new Rule(15) }
+            };
+
+            ICheckout checkout = new Checkout(pricingRules);
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("C");
+            checkout.Scan("D");
+
+            Assert.Equal(115, checkout.GetTotalPrice()); // 50 (A) + 30 (B) + 20 (C) + 15 (D)
+        }
     }
 }
